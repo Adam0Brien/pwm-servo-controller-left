7fe167a1e67964afebb09f802011bf363ae3e505
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-02-25 16:47:12 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,18 @@ radio.setGroup(2)
 basic.showLeds(`
     . . # . .
     . . # . .
-    . . . . .
+    . . # . .
     . . # . .
     . . # # #
     `)
 basic.forever(function () {
     led.plot(2, 2)
     led.unplot(2, 2)
-    if (input.rotation(Rotation.Roll) >= 0) {
-        radio.sendValue("leftf", Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), 0, 90), 0, 90, 90, 180)))
-    } else {
-        radio.sendValue("leftr", Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -90, 0), 0, -90, 0, 89)))
+    if (input.rotation(Rotation.Pitch) < 50 && input.rotation(Rotation.Pitch) > -50) {
+        radio.sendValue("left", Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -90, 90), 0, 90, 90, 110)))
     }
+})
+basic.forever(function () {
+    radio.setGroup(2)
+    radio.sendNumber(1)
 })
