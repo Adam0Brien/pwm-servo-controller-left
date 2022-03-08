fbd3c42a77af751f50d65fd6399ee57fc7d247f3
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-03-08 21:23:16 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-radio.setGroup(2)
+radio.setGroup(1)
 basic.showLeds(`
     . . # . .
     . . # . .
@@ -10,10 +10,8 @@ basic.forever(function () {
     led.plot(2, 2)
     led.unplot(2, 2)
     if (input.rotation(Rotation.Pitch) < 50 && input.rotation(Rotation.Pitch) > -50) {
-        radio.sendValue("left", Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -90, 90), 0, 90, 90, 110)))
+        radio.sendValue("left", Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -90, 90), 0, 90, 90, 120)))
+        serial.writeLine("" + (Math.round(Math.map(Math.constrain(input.rotation(Rotation.Roll), -90, 90), 0, 90, 90, 120))))
+        radio.sendNumber(1)
     }
-})
-basic.forever(function () {
-    radio.setGroup(2)
-    radio.sendNumber(1)
 })
